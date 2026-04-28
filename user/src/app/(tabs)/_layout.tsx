b2e4b6f1660a766bbdd6e0e114862f90183d5c69
Nur-Fitriana <ftrianaaaa55@gmail.com>
2026-04-29 05:01:41 +0700

    fix: memperbaiki error layar merah pada navigasi

--- user/src/app/(tabs)/_layout.tsx
+++ user/src/app/(tabs)/_layout.tsx
@@ -1,33 +1,29 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: '#0080FF' }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Beranda',
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="fasilitas"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Wahana',
+          tabBarIcon: ({ color }) => <Ionicons name="water" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="tiket"
+        options={{
+          title: 'Tiket',
+          tabBarIcon: ({ color }) => <Ionicons name="ticket" size={24} color={color} />,
         }}
       />
     </Tabs>
