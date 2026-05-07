f7f358fee35a57a66f1d3afae980d265ba740490
cindyayu052-hue <cindyayu052@gmail.com>
2026-05-07 14:58:09 +0700

    fix: perbaikan facebook di kontak dan media sosial

--- cms/src/app/lokasi/page.tsx
+++ cms/src/app/lokasi/page.tsx
@@ -9,7 +9,7 @@ export default function LocationContactManagement() {
     phone: "",
     email: "",
     instagram: "",
-    facebook: "Slanik Waterpark Official",
+    facebook: "",
     whatsapp: "",
   });
 
