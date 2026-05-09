90d037f637d4e5099769228e587871b64a226602
Trilia01 <Trilia_gita_amanda@teknokrat.ac.id>
2026-05-10 01:36:26 +0700

    refactor: add prisma service and common helpers

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+
+    const adapter = new PrismaPg({
+      connectionString,
+    });
+
+    super({
+      adapter,
+    });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
