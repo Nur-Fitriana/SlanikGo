3ac7c9ec473d0aaae710e315f93563005bcdcd22
Trilia01 <Trilia_gita_amanda@teknokrat.ac.id>
2026-05-30 22:11:06 +0700

    Refactor(fasilitas): update controller routes

--- api/gateway/src/fasilitas/fasilitas.controller.ts
+++ api/gateway/src/fasilitas/fasilitas.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { FasilitasService } from './fasilitas.service';
+import { CreateFasilitasDto } from './dto/create-fasilitas.dto';
+import { UpdateFasilitasDto } from './dto/update-fasilitas.dto';
 
 @Controller('fasilitas')
-export class FasilitasController {}
+export class FasilitasController {
+  constructor(private readonly fasilitasService: FasilitasService) {}
+
+  @Post()
+  create(@Body() createFasilitasDto: CreateFasilitasDto) {
+    return this.fasilitasService.create(createFasilitasDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.fasilitasService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.fasilitasService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateFasilitasDto: UpdateFasilitasDto,
+  ) {
+    return this.fasilitasService.update(id, updateFasilitasDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.fasilitasService.remove(id);
+  }
+}
