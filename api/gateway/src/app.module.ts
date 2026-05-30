b61a6ca8f8f08a9a2c33c26c9fe96317c8518cd4
Trilia01 <Trilia_gita_amanda@teknokrat.ac.id>
2026-05-30 22:08:15 +0700

    Refactor: update root app module imports

--- api/gateway/src/app.module.ts
+++ api/gateway/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
+import { FasilitasModule } from './fasilitas/fasilitas.module';
+import { GaleriModule } from './galeri/galeri.module';
+import { InfoModule } from './info/info.module';
+import { TiketModule } from './tiket/tiket.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [AuthModule, FasilitasModule, GaleriModule, InfoModule, TiketModule],
   controllers: [AppController],
   providers: [AppService],
 })
