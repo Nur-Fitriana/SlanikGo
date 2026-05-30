6321a34a2867f2cf753df5b501d4a2338c828225
Trilia01 <Trilia_gita_amanda@teknokrat.ac.id>
2026-05-30 22:16:40 +0700

    Refactor(tiket): update ticketing routes

--- api/gateway/src/tiket/tiket.controller.ts
+++ api/gateway/src/tiket/tiket.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { TiketService } from './tiket.service';
+import { CreateTiketDto } from './dto/create-tiket.dto';
+import { UpdateTiketDto } from './dto/update-tiket.dto';
 
 @Controller('tiket')
-export class TiketController {}
+export class TiketController {
+  constructor(private readonly tiketService: TiketService) {}
+
+  @Post()
+  create(@Body() createTiketDto: CreateTiketDto) {
+    return this.tiketService.create(createTiketDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.tiketService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.tiketService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateTiketDto: UpdateTiketDto,
+  ) {
+    return this.tiketService.update(id, updateTiketDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.tiketService.remove(id);
+  }
+}
