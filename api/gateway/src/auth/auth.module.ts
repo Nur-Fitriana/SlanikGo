9a4e97184e35333fdf3a5392b2859ed461edd721
Trilia01 <Trilia_gita_amanda@teknokrat.ac.id>
2026-05-30 21:37:07 +0700

    Chore(auth): update dependencies in auth module

--- api/gateway/src/auth/auth.module.ts
+++ api/gateway/src/auth/auth.module.ts
@@ -1,7 +1,16 @@
+// auth.module.ts
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtAccessStrategy } from './strategies/jwt-access.strategy';
+import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
 
 @Module({
-  controllers: [AuthController]
+  imports: [PassportModule, JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [AuthService, JwtAccessStrategy, JwtRefreshStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
