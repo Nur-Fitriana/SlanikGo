f74592f9866212ec704c5995b85bc8cf30d1c240
Trilia01 <Trilia_gita_amanda@teknokrat.ac.id>
2026-05-30 22:07:55 +0700

    Refactor: update main gateway entry point configuration

--- api/gateway/src/main.ts
+++ api/gateway/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Aktifkan validator global untuk DTO
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  // Jika aplikasi ini berjalan di balik gateway/routing tertentu, definisikan prefix di sini jika diperlukan
+  // app.setGlobalPrefix('api');
+
+  await app.listen(3000);
+  console.log(`Application is running on: http://localhost:3000`);
 }
 bootstrap();
